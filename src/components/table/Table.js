58fa7d4ade0a1c0bf67e932d844575024f818f55
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-12-13 05:33:02 +0600

    state

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -5,6 +5,7 @@ import {resizeHandler} from './table.resize';
 import {TableSelection} from './TableSelection';
 import {matrix} from './table.functions';
 import {isCell, nextSelector, shouldResize} from './table.functions';
+import * as actions from '@/redux/actions'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -18,7 +19,7 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable()
+    return createTable(20, this.store.getState())
   }
 
   prepare() {
@@ -32,6 +33,7 @@ export class Table extends ExcelComponent {
 
     this.$on('formula:input', text => {
       this.selection.current.text(text)
+      this.updateTextInStore(text)
     })
 
     this.$on('formula:blur', () => {
@@ -44,9 +46,18 @@ export class Table extends ExcelComponent {
     this.$emit('table:select', $cell)
   }
 
+  async resizeTable(event) {
+    try {
+      const data = await resizeHandler(this.$root, event)
+      this.$dispatch(actions.tableResize(data))
+    } catch (e) {
+      console.warn('Resize error', e.message)
+    }
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
-      resizeHandler(this.$root, event)
+      this.resizeTable(event)
     } else if (isCell(event)) {
       const $target = $(event.target)
       if (event.shiftKey) {
@@ -55,7 +66,7 @@ export class Table extends ExcelComponent {
 
         this.selection.selectGroup(cells)
       } else {
-        this.selection.select($target)
+        this.selectCell($target)
       }
     }
   }
@@ -81,8 +92,15 @@ export class Table extends ExcelComponent {
     }
   }
 
+  updateTextInStore(value) {
+    this.$dispatch(actions.changeText({
+      id: this.selection.current.id(),
+      value
+    }))
+  }
+
   onInput(event) {
-    this.$emit('table:input', $(event.target))
+    this.updateTextInStore($(event.target).text())
   }
 }
 
